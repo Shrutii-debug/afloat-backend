e50b7ba3891e78790c19146ff8b2bdb9ae185c87
Shrutii-debug <shrutiagarwal1467@gmail.com>
2026-02-17 22:24:02 +0530

    creating conf and main files

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+try {
+  await pool.connect();
+  console.log("🟢 PostgreSQL Connected");
+} catch (err) {
+  console.error("🔴 DB Connection Error:", err);
+}
