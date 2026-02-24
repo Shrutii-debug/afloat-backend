abc89149cfcae96374013ea8fb44e597262127ab
Shrutii-debug <shrutiagarwal1467@gmail.com>
2026-02-24 23:26:41 +0530

    initial middlewares

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import ApiError from "../utils/ApiError.js";
+import { verifyToken } from "../utils/jwt.js";
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+
+    if (!header || !header.startsWith("Bearer "))
+      throw new ApiError(401, "Unauthorized");
+
+    const token = header.split(" ")[1];
+    const decoded = verifyToken(token);
+
+    req.user = decoded;
+    next();
+  } catch {
+    next(new ApiError(401, "Invalid or expired token"));
+  }
+};
+
+export default authMiddleware;
---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,9 @@
+import ApiError from "../utils/ApiError.js";
+
+const roleMiddleware = (role) => (req, res, next) => {
+  if (req.user.role !== role)
+    return next(new ApiError(403, "Access denied"));
+  next();
+};
+
+export default roleMiddleware;
