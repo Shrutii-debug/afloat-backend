92cf100a813e2715927d9d79263a56e894b34982
Shrutii-debug <shrutiagarwal1467@gmail.com>
2026-02-24 22:17:48 +0530

    deleted the models

--- src/models/opportunity.model.js
+++
@@ -1,8 +0,0 @@
-import { pool } from "../config/db.js";
-
-export const getAllOpportunities = async () => {
-  const { rows } = await pool.query(
-    `SELECT * FROM opportunities ORDER BY deadline ASC`
-  );
-  return rows;
-};
--- src/models/preference.model.js
+++
@@ -1,9 +0,0 @@
-import { pool } from "../config/db.js";
-
-export const findPreferences = async (studentId) => {
-  const { rows } = await pool.query(
-    `SELECT * FROM student_preferences WHERE student_id=$1`,
-    [studentId]
-  );
-  return rows[0];
-};
